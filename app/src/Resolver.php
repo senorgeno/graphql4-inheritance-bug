13b3f4e3bfc306cc878ec14d1c58200c8216005f
Gene Dower <genedower@gmail.com>
2021-01-12 11:39:51 +1300

    Use pagaination resolver

--- app/src/Resolver.php
+++ app/src/Resolver.php
@@ -3,7 +3,7 @@
 
 namespace GenedTest\Resolvers;
 
-
+use GenedTest\Models\Restaurant;
 use SilverStripe\GraphQL\Schema\Plugin\PaginationPlugin;
 
 class Resolver
@@ -20,4 +20,42 @@ class Resolver
             );
         };
     }
+
+    public static function paginateRestaurants($maxLimit)
+    {
+        //return Restaurant::get();
+        return function ($obj, $args) {
+
+            // $limitedResults = array_slice($obj, $args['offset'], $args['limit']);
+            return PaginationPlugin::createPaginationResult(
+                count($obj),
+                Restaurant::get(),
+                $args['limit'],
+                $args['offset']
+            );
+        };
+    }
+
+//    public static function paginateRestaurants(array $context): Closure
+//    {
+//        $maxLimit = $context['maxLimit'];
+//
+//        return function ($list, array $args, array $context, ResolveInfo $info) use ($maxLimit) {
+//            if (!$list instanceof Limitable) {
+//                //var_dump(count($list)); die();
+//                return PaginationPlugin::createPaginationResult(count($list), $list, $maxLimit, 0);
+//            }
+//
+//            $offset = $args['offset'];
+//            $limit = $args['limit'];
+//            $total = $list->count();
+//
+//            $limit = min($limit, $maxLimit);
+//
+//            // Apply limit
+//            /* @var Limitable $list */
+//            $limitedList = $list->limit($limit, $offset);
+//            return PaginationPlugin::createPaginationResult($total, $limitedList, $limit, $offset);
+//        };
+//    }
 }
